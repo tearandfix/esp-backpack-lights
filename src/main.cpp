45d4ee5aca37fe76f8b8672d850cdab59a2caef1
tearandfix <79838675+tearandfix@users.noreply.github.com>
2024-08-29 12:04:47 +0300

    Example neopixels usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,86 @@
-/*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// Released under the GPLv3 license to match the rest of the
+// Adafruit NeoPixel library
 
-#include "Arduino.h"
+#include <Adafruit_NeoPixel.h>
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN        3
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 45 // Popular NeoPixel ring size
+
+// When setting up the NeoPixel library, we tell it how many pixels,
+// and which pin to use to send signals. Note that for older NeoPixel
+// strips you might need to change the third parameter -- see the
+// strandtest example for more information on possible values.
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+#define DELAYVAL 10 // Time (in milliseconds) to pause between pixels
+
+void setup() {
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  // wait for a second
-  delay(1000);
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-   // wait for a second
-  delay(1000);
+void ua_flag() {
+  pixels.clear(); // Set all pixel colors to 'off'
+  auto blue = pixels.Color(0, 0, 255);
+  auto yellow = pixels.Color(128, 128, 0);
+  pixels.fill(blue, 7 + 7, 7 + 10 + 7);
+  pixels.fill(yellow, 0, 7 + 7);
+  pixels.fill(yellow, 7 + 14 + 10 + 7, 7);
+
+  pixels.show();
+}
+
+void set_left(uint32_t color) {
+  pixels.fill(color, 0, 3);
+  pixels.fill(color, 7 + 14 + 5, 5 + 14);
+  pixels.show();
+}
+
+void set_right(uint32_t color) {
+  pixels.fill(color, 4, 3 + 14 + 5);
+  pixels.show();
+}
+
+void hazard_flash() {
+  auto orange = pixels.Color(255, 100, 0);
+  auto off = pixels.Color(0, 0, 0);
+  const auto pause = 50;
+  pixels.clear();
+  for (int i=0; i<4; i++) {
+    set_left(orange);
+    delay(pause);
+    set_left(off);
+    delay(pause);
+  }
+  for (int i=0; i<4; i++) {
+    set_right(orange);
+    delay(pause);
+    set_right(off);
+    delay(pause);
+  }
+}
+
+void loop() {
+  hazard_flash();
+  /*ua_flag();*/
+  /*delay(2000);*/
+
+  /*pixels.clear(); // Set all pixel colors to 'off'*/
+
+  // The first NeoPixel in a strand is #0, second is 1, all the way up
+  // to the count of pixels minus one.
+  /*for(int i=0; i<NUMPIXELS; i++) { // For each pixel...*/
+  /**/
+  /*  // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255*/
+  /*  // Here we're using a moderately bright green color:*/
+  /*  pixels.setPixelColor(i, pixels.Color(0, 0, 255));*/
+  /**/
+  /*  pixels.show();   // Send the updated pixel colors to the hardware.*/
+  /**/
+  /*  delay(DELAYVAL); // Pause before next pass through loop*/
+  /*  pixels.setPixelColor(i, pixels.Color(0, 0, 0));*/
+  /*}*/
 }
